a647b1e6c383f6c97ea11cf8b882919fa8e70d40
aeonyuonmiller <aym1@mail.com>
2021-01-11 00:12:31 +0100

    gsap work smooth

--- pages/insert-name-here.js
+++ pages/insert-name-here.js
@@ -53,39 +53,43 @@ const variants = {
 
 export default function INH() {
 
+// make gsap work smooth
+const handleScroll = () => {}
+useEffect(() => {
+window.addEventListener('scroll', handleScroll);
+return () => window.removeEventListener('scroll', handleScroll);
+});
+
 
 // Resize Hook
 function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({
-    width: undefined,
-    height: undefined,
-  });
 
-  useEffect(() => {
-    // only execute all the code below in client side
-    if (typeof window !== 'undefined') {
-      // Handler to call on window resize
-      function handleResize() {
-        // Set window width/height to state
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      }
-    
-      // Add event listener
-      window.addEventListener("resize", handleResize);
-     
-      // Call handler right away so state gets updated with initial window size
-      handleResize();
-    
-      // Remove event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
+const [windowSize, setWindowSize] = useState({
+  width: undefined,
+  height: undefined,
+});
+
+useEffect(() => {
+  // only execute all the code below in client side
+  if (typeof window !== 'undefined') {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     }
-  }, []); // Empty array ensures that effect is only run on mount
-  return windowSize;
+  
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+   
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+  
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }
+}, []); // Empty array ensures effect is only run on mount
+return windowSize;
 }
 
 
--- pages/rhythm.js
+++ pages/rhythm.js
@@ -51,13 +51,21 @@ const variants = {
   transition: { ease, duration: 1 },
 }
 
+
+
 export default function RHYM() {
+
+// make gsap work smooth
+  const handleScroll = () => {}
+  useEffect(() => {
+  window.addEventListener('scroll', handleScroll);
+  return () => window.removeEventListener('scroll', handleScroll);
+  });
 
 
 // Resize Hook
 function useWindowSize() {
-  // Initialize state with undefined width/height so server and client renders match
-  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  
   const [windowSize, setWindowSize] = useState({
     width: undefined,
     height: undefined,
@@ -66,9 +74,7 @@ function useWindowSize() {
   useEffect(() => {
     // only execute all the code below in client side
     if (typeof window !== 'undefined') {
-      // Handler to call on window resize
       function handleResize() {
-        // Set window width/height to state
         setWindowSize({
           width: window.innerWidth,
           height: window.innerHeight,
@@ -84,7 +90,7 @@ function useWindowSize() {
       // Remove event listener on cleanup
       return () => window.removeEventListener("resize", handleResize);
     }
-  }, []); // Empty array ensures that effect is only run on mount
+  }, []); // Empty array ensures effect is only run on mount
   return windowSize;
 }
 
